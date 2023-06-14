0bb4c7611fd3321adcdbd23d299a0a53a9c4f35c
Denis Paasch <denis.paasch@gmail.com>
2023-06-14 15:29:35 +0200

    Avoid lombok usage in entities (part 2)

--- infrastructure/src/main/java/be/dpa/bootiful/activities/infrastructure/jpa/entities/ParticipantEntity.java
+++ infrastructure/src/main/java/be/dpa/bootiful/activities/infrastructure/jpa/entities/ParticipantEntity.java
@@ -1,13 +1,15 @@
 package be.dpa.bootiful.activities.infrastructure.jpa.entities;
 
-import lombok.Data;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.Objects;
 
 /**
  * Participant entity.
@@ -15,13 +17,13 @@ import jakarta.validation.constraints.Size;
  * @author denis
  */
 @Entity(name = "participant")
-@Data
 public class ParticipantEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NaturalId
     @NotNull(message = "The alternate key is mandatory")
     @Size(min = 1, max = 255)
     private String alternateKey;
@@ -33,4 +35,64 @@ public class ParticipantEntity {
     @NotNull(message = "The last name is mandatory")
     @Size(min = 1, max = 255)
     private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAlternateKey() {
+        return alternateKey;
+    }
+
+    public void setAlternateKey(String alternateKey) {
+        this.alternateKey = alternateKey;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(alternateKey);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ActivityEntity other = (ActivityEntity) obj;
+        return StringUtils.equals(alternateKey, other.getAlternateKey());
+    }
+
+    @Override
+    public String toString() {
+        return "ParticipantEntity(id=" + this.getId()
+                + ", alternateKey=" + this.getAlternateKey()
+                + ", firstName=" + this.getFirstName()
+                + ", lastName=" + this.getLastName() + ")";
+    }
 }
